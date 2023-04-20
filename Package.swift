b66791351cfc3a12d9a16cd9f7264e3e33311867
Andrew Graff <andrewgraff@Andrews-MacBook-Pro.local>
2023-04-20 15:10:22 -0700

    Fixing resources name

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@
 
 import PackageDescription
 let packageName = "OpenTok"
+let targertsName = "OpenTokResources"
 
 let package = Package(
     name: packageName,
@@ -10,7 +11,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: packageName,
-            targets: ["TechnologiesOpenTok"]),
+            targets: [targertsName]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,7 +21,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "TechnologiesOpenTok",
+            name: targertsName,
             dependencies: [
                 .target(name: packageName)
             ],
